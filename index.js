557d3fc20cc242efde80bd73a40a48eb0b9068ea
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-09-22 08:48:13 +0530

    GEt the exact Coordinates of card element in page

---
+++ index.js
@@ -0,0 +1,52 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.getElementById('times')
+
+// loveMe.addEventListener('dbclick',(e)=>{
+//     console.log(123);
+// })
+
+let clickTime = 0
+
+//  My Method for double Click
+
+// loveMe.addEventListener('click',(e)=>{
+//    clickTime++;
+
+//    setTimeout(()=>{
+//         clickTime = 0;
+//    },800)
+
+//    if(clickTime === 2){
+//     console.log('123');
+//    }
+// })
+
+loveMe.addEventListener('click',(e)=> {
+    if(clickTime ===0) {
+        clickTime = new Date().getTime()
+    } else {
+        if((new Date().getTime() - clickTime) < 800){
+           createHeart(e)
+           clickTime = 0
+        }else {
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    console.log(xInside,yInside);
+}
